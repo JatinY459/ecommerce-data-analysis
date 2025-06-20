f7b498074e1ecda842aadc64122f132b0679cdbb
JatinY459 <14jatinyadav@gmail.com>
2025-06-20 17:42:35 +0530

    fixed path errors + updated app.py & import_data util function

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,3 +1,12 @@
+# fixing the path as root directory is 'dashboard'
+# import os
+# import sys
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# or use this command instead of this code: $env:PYTHONPATH="."; streamlit run dashboard/app.py
+
+
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -7,7 +16,7 @@ import seaborn as sns
 from scripts.data_cleaning import import_data
 
 # Import data
-orders, order_items, customers, payments, products = import_data()
+orders, order_items, customers, payments, products = import_data(dashboard=True)
 
 st.title("E-commerce Data Analysis Dashboard")
 st.markdown("<p style='font-size:1.5rem; font-weight:700;'>Based on Brazilian E-commerce Platform Insights, Trends, and Behavior Patterns</p>", unsafe_allow_html=True)
--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -2,7 +2,22 @@ import pandas as pd
 import numpy as np
 
 
-def import_data():
+def import_data(dashboard=False):
+    if dashboard:
+            date_cols = ["order_purchase_timestamp", "order_approved_at", "order_delivered_timestamp", "order_estimated_delivery_date"]
+
+            orders = pd.read_csv('data/processed/orders.csv')
+            orders[date_cols] = orders[date_cols].apply(pd.to_datetime, errors='coerce')  # Safely parse all, fallback if weird values
+
+            orders["delivery_time_gap"] = pd.to_timedelta(orders["delivery_time_gap"], errors='coerce')
+
+            order_items = pd.read_csv('data/processed/order_items.csv')
+            customers = pd.read_csv('data/processed/customers.csv')
+            payments = pd.read_csv('data/processed/payments.csv')
+            products = pd.read_csv('data/processed/products.csv')
+
+            return orders, order_items, customers, payments, products
+    
     date_cols = ["order_purchase_timestamp", "order_approved_at", "order_delivered_timestamp", "order_estimated_delivery_date"]
 
     orders = pd.read_csv('../data/processed/orders.csv')
