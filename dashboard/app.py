637e619b097eb3b50a4b0b6c1b3289803e17a041
JatinY459 <14jatinyadav@gmail.com>
2025-06-20 17:08:00 +0530

    updated homepage in dashboard app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,3 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from scripts.data_cleaning import import_data
+
+# Import data
+orders, order_items, customers, payments, products = import_data()
+
+st.title("E-commerce Data Analysis Dashboard")
+st.markdown("<p style='font-size:1.5rem; font-weight:700;'>Based on Brazilian E-commerce Platform Insights, Trends, and Behavior Patterns</p>", unsafe_allow_html=True)
+
+st.markdown("""
+This dashboard presents a deep-dive exploratory analysis of a large-scale e-commerce dataset.
+It uncovers consumer behavior, delivery performance, product pricing, and payment trends.
+Use the sidebar to navigate through focused insights from different aspects of the platform's operations.
+""")
+
