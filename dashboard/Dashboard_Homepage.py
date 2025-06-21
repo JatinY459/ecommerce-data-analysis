c4b0d6c1f57fd5b7bf4f00fdd71e268aea2eac1d
JatinY459 <14jatinyadav@gmail.com>
2025-06-21 18:24:45 +0530

    added a data cleaning measure in 02_data_cleaning

--- dashboard/Dashboard_Homepage.py
+++ dashboard/Dashboard_Homepage.py
@@ -11,7 +11,7 @@ import pandas as pd
 from scripts.data_cleaning import import_data
 
 
-st.set_page_config(page_title="E-commerce EDA", page_icon="📑")
+st.set_page_config(page_title="E-commerce EDA", page_icon="📑", initial_sidebar_state="collapsed")
 
 
 # styles
