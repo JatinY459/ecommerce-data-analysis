faf5f7f3ab573f2c2eebf07ce9efc91aa3b03274
JatinY459 <14jatinyadav@gmail.com>
2025-06-23 17:20:18 +0530

    added delivery time analysis in dashboard page1

--- dashboard/pages/1_Orders_And_Delivery_Trends.py
+++ dashboard/pages/1_Orders_And_Delivery_Trends.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 
@@ -68,3 +69,63 @@ fig.update_layout(
     yaxis=dict(tickformat=',d')
 )
 st.plotly_chart(fig)
+
+# delivery time Analysis
+st.markdown("""<h2 style='font-size:1.5rem; font-weight:700;'>Delivery Time Analysis</h2>""", unsafe_allow_html=True)
+
+x_values = pd.to_numeric(orders["delivery_time_gap_hrs"], errors="coerce").dropna()
+
+fig = px.histogram(x=x_values, nbins=20, opacity=0.6, color_discrete_sequence=["#636EFA"])
+
+hist, bin_edges = np.histogram(x_values, bins=20, density=True)
+y_max = hist.max()
+
+# line for on time deliveries, x=0
+fig.add_vline(
+    x=0,
+    line=dict(color="red", width=3, dash="dash"),
+    annotation_text="On Time",
+    annotation_position="top",
+    annotation_font_color="red",
+    annotation_font_size=20
+)
+# adding label for late & early delivery
+fig.add_annotation(
+    x=x_values.max()* 0.9,
+    y=y_max * 500,
+    text="Early Deliveries",
+    showarrow=False,
+    font=dict(color="yellow", size=20),
+    yshift=100
+)
+fig.add_annotation(
+    x=x_values.min() * 0.7,
+    y=y_max * 500,
+    text="Late Deliveries",
+    showarrow=False,
+    font=dict(color="orange", size=20),
+    yshift=100
+)
+fig.update_layout(
+    title="Delivery Time Gap Distribution",
+    xaxis_title="Delivery Time Gap (hrs)",
+    yaxis_title="Density",
+    template="simple_white",
+    legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ),
+    font=dict(size=14)
+)
+fig.update_traces(marker_line_width=1, marker_line_color="white") 
+
+st.plotly_chart(fig)
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Early Delivered Orders (%)", f"{(x_values > 12).mean() * 100:.1f}%")
+col2.metric("Late Delivered Orders (%)", f"{(x_values < -12).mean() * 100:.1f}%")
+col3.metric("Delivered on Estimated Day (%)", f"{(x_values.between(-12,12)).mean() * 100:.1f}%")
+col4.metric("Average Delivery Time (hrs)", f"{x_values.mean():.2f}")
