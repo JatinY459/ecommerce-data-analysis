d5cd129680e737019117166b4f464decfd95f97a
JatinY459 <14jatinyadav@gmail.com>
2025-06-28 19:50:25 +0530

    updated page 3 in dashboard

--- dashboard/pages/3_Product_and_Category_Insights.py
+++ dashboard/pages/3_Product_and_Category_Insights.py
@@ -36,6 +36,9 @@ filtered_orders = filtered_orders[filtered_orders["order_status"].isin(selected_
 filtered_customers = customers[customers["customer_id"].isin(filtered_orders["customer_id"])]
 filtered_customers = filtered_customers[filtered_customers["customer_state"].isin(selected_states)]
 
+filtered_order_items = order_items[order_items["order_id"].isin(filtered_orders["order_id"])]
+filtered_products = products[products["product_id"].isin(filtered_order_items["product_id"])]
+
 # mapping tool for customer by region
 state_to_region = {
     state: region
@@ -43,8 +46,11 @@ state_to_region = {
     for state in states
 }
 filtered_customers["customer_region"] = filtered_customers["customer_state"].map(state_to_region)
+
+# merged DFs
 customers_orders = filtered_orders.merge(filtered_customers, on="customer_id")
-cust_orders_items = customers_orders.merge(order_items, on="order_id")
+cust_orders_items = customers_orders.merge(filtered_order_items, on="order_id")
+products_order_items = filtered_order_items.merge(filtered_products, on="product_id")
 
 # resources
 customers_by_region = filtered_customers.groupby("customer_region")["customer_id"].nunique().reset_index().rename(columns={"customer_id": "orders_count"})
@@ -59,27 +65,56 @@ ship_price_by_state = ship_price_by_state.sort_values(by="avg_shipping_charges",
 ship_price_by_region = cust_orders_items.groupby("customer_region")["shipping_charges"].mean().reset_index().rename(columns={"shipping_charges": "avg_shipping_charges"})
 ship_price_by_region = ship_price_by_region.sort_values(by="avg_shipping_charges", ascending=False)
 
+# for this one:
+products_by_category = filtered_products.groupby("product_category_name")["product_id"].nunique().reset_index().rename(columns={"product_id": "products_count"})
+products_by_category = products_by_category.sort_values(by="products_count", ascending=False)
+
+products_price_by_category = products_order_items.groupby("product_category_name")["price"].mean().reset_index()
+products_price_by_category = products_price_by_category.sort_values(by="price", ascending=False)
+
 # dashboard content
 st.title("Product & Category Analysis")
 st.markdown("""
-This page explores product performance, category trends, and regional influences on product sales and categories.  
-Understand the trends in product performance and category distribution in the market.
+This section explores the landscape of products and categories in the e-commerce platform.  
+We examine category popularity, pricing patterns, and product specifications (weight, dimensions), revealing inventory and logistics implications.
 <p style='font-style:italic; margin-top:-8px; font-weight:700;'>Note: Use side bar for filters</p>
 """, unsafe_allow_html=True)
 col1, col2, col3 = st.columns(3)
-col1.metric("Total Unique Customers Ids", f"{filtered_customers.shape[0]:,}")
-col2.metric("Cities with Orders", f"{(filtered_customers['customer_city'].nunique())}")
-col3.metric("Top State", f"{customers_by_state.loc[customers_by_state['orders_count'].idxmax(), 'customer_state']} ({state_names_dict[customers_by_state.loc[customers_by_state['orders_count'].idxmax(), 'customer_state']]})")
+col1.metric("Total Product Categories", f"{filtered_products['product_category_name'].nunique():,}")
+col2.metric("Number of Unique Products", f"{filtered_products['product_id'].nunique():,}")
+col3.metric("Avg Product Price", f"{filtered_order_items['price'].mean():,.2f}")
+
+st.plotly_chart(
+    px.histogram(
+        filtered_order_items,
+        x="price",
+        nbins=50,
+        title="Product Price Distribution",
+        labels={"price": "Product Price"},
+        color_discrete_sequence=px.colors.qualitative.Pastel,
+        template="plotly_white"
+    ).update_layout(
+        xaxis_title="Product Price",
+        yaxis_title="Count",
+        title_x=0.5,
+        font=dict(size=14),
+        bargap=0.1,
+        bargroupgap=0.1,
+        xaxis=dict(tickformat=',.0f', ticks='outside', showline=True, linewidth=1, linecolor='black'),
+        yaxis=dict(ticks='outside', showline=True, linewidth=1, linecolor='black')
+    )
+)
 
 col1, col2 = st.columns(2)
+
 fig = px.bar(
-    customers_by_region,
-    x="customer_region",
-    y="orders_count",
-    color="customer_region",
-    title="Region-Wise Order Distribution",
-    text='orders_count',
-    labels={'customer_region': 'Region', 'orders_count': 'Number of Orders'},
+    products_by_category.head(5),
+    x="product_category_name",
+    y="products_count",
+    color="product_category_name",
+    title="Category-Wise Product Distribution (Top 5)",
+    text='products_count',
+    labels={'product_category_name': 'Category', 'products_count': 'Number of Products'},
     template="plotly_white"
 )
 fig.update_traces(textposition='outside')
@@ -87,66 +122,86 @@ fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
 col1.plotly_chart(fig)
 
 fig = px.pie(
-    customers_by_region,
-    names='customer_region',
-    values='orders_count',
-    title='Region Orders Distribution',
-    labels={'customer_region': 'Region', 'orders_count': 'Number of Orders'},
+    pd.concat([products_by_category.head(5), pd.DataFrame({'product_category_name': ['Others'], 'products_count': [products_by_category['products_count'].sum() - products_by_category.head(5)['products_count'].sum()]})]),
+    names='product_category_name',
+    values='products_count',
+    title='Product Categories Top 5 vs Others',
+    labels={'product_category_name': 'Category', 'products_count': 'Number of Products'},
     hole=0.4,
     color_discrete_sequence=px.colors.qualitative.Pastel
 )
 fig.update_traces(textposition='inside', textinfo='percent+label', hovertemplate="<b>%{label}</b><br>Count: %{value}<br>Percentage: %{percent}")
 col2.plotly_chart(fig)
 
-with st.expander("Region-Wise Order Distribution Summary"):
-    st.markdown("""- The **South-East region dominates order volume**, accounting for around 69% of total orders, driven by economically strong states like SP, RJ, and MG.
-- The **North region contributes just 1.8%**, reflecting its less population and poor infrastructure.
-- The distribution shows clear **regional concentration** of e-commerce usage in urban hubs.""")
-    col1, col2, col3, col4 = st.columns([1, 2, 2, 1])
-    col2.metric("highest volume region to second highest", f"{(customers_by_state.iloc[0]["orders_count"]/customers_by_state.iloc[1]["orders_count"]) * 100:.2f} %")
-    col3.metric("highest volume region to lowest", f"{(customers_by_state.iloc[0]["orders_count"]/customers_by_state.iloc[-1]["orders_count"]) * 100:.2f} %")
+with st.expander("Products Categories Top 5 vs Others Summary"):
+    st.markdown("""Write the summaries & Metrics/KPIs here""")
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Mean no. of Products [Including Toys]", f"{products_by_category['products_count'].mean():.2f}")
+    col2.metric("Mean no. of Products [Excluding Toys]", f"{products_by_category[products_by_category['product_category_name'] !='toys']['products_count'].mean():.2f}")
 
-agg_df = filtered_customers.groupby(["customer_region", "customer_state"]).agg(order_count=("customer_id", "count")).reset_index()
-fig = px.treemap(
-    agg_df,
-    path=["customer_region", "customer_state"],
-    values="order_count",
-    title="Orders Volume by Region & State Treemap",
-    color="order_count",
-    color_continuous_scale=px.colors.sequential.Sunsetdark,
+# category distro
+st.markdown("""<h2 style='font-size:1.5rem; font-weight:700;'>Product Categories Distribution</h2>
+            Options:""", unsafe_allow_html=True)
+col1, col2, col3, col4 = st.columns([1, 3, 3, 1])
+category_view = col2.radio(
+    "View Categories by Product Count",
+    options=["Top N", "Bottom N"],
+    index=0,
+    horizontal=True
 )
-fig.update_traces(
-    textinfo="label+value+percent parent",
-    hovertemplate="<b>%{label}</b><br>Orders: %{value}<br>% of Parent: %{percentParent:.2%}<br>% of Total: %{percentRoot:.2%}<extra></extra>"
+n = col3.slider(
+    f"Select number of categories to display",
+    min_value=1,
+    max_value=min(35, 69),
+    value=20
 )
-fig.update_layout(
-    title_x=0.4,
-    margin=dict(t=50, l=25, r=25, b=25),
-    font=dict(size=14),
-    coloraxis_colorbar=dict(title="Order Count")
-)
-st.plotly_chart(fig)
-with st.expander("Region & State Orders Volume Summary"):
-    st.markdown("""
-- The treemap reinforces that **SP, RJ, and MG are the top-performing states**, leading order volume significantly.
-- Northern states like RR, AC, and AP combined contribute just **0.02% of total orders**.
-- Federal District (DF) despite being very small contributes about 2% of total orders due to its administrative importance.
-    """)
+if category_view == "Top N":
+    fig = px.bar(
+        products_by_category.iloc[1:n+1],
+        x="product_category_name",
+        y="products_count",
+        color="product_category_name",
+        title=f"Product Categories Distribution (Top {n}) [Excluding Toys]",
+        text='products_count',
+        labels={'product_category_name': 'Category', 'products_count': 'Number of Products'},
+        template="plotly_white"
+    )
+    fig.update_traces(textposition='outside')
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    st.plotly_chart(fig)
+
+if category_view == "Bottom N":
+    fig = px.bar(
+        products_by_category.iloc[-n:-1],
+        x="product_category_name",
+        y="products_count",
+        color="product_category_name",
+        title=f"Product Categories Distribution (Bottom {n})",
+        text='products_count',
+        labels={'product_category_name': 'Category', 'products_count': 'Number of Products'},
+        template="plotly_white"
+    )
+    fig.update_traces(textposition='outside')
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    st.plotly_chart(fig)
+
+with st.expander("Top Product Categories Distribution Summary"):
+    st.markdown("""Write the summaries & Metrics/KPIs here""")
     col1, col2, col3, col4, col5 = st.columns([1, 3, 3, 3, 1])
-    col2.metric("Top 3 States Combined %", f"{(customers_by_state.head(3)['orders_count'].sum()) / (customers_by_state['orders_count'].sum()) * 100:.2f} %")
-    col3.metric("Bottom 3 Contributing States Combined %", f"{(customers_by_state.tail(3)['orders_count'].sum()) / (customers_by_state['orders_count'].sum()) * 100:.2f} %")
-    col4.metric("Orders from Least Contributing States", f"{customers_by_state.tail(3)['orders_count'].sum()}")
+    col2.metric("Mean number of Products (filtered view)", f"{products_by_category.iloc[1:n+1]["products_count"].mean() if category_view=="Top N" else products_by_category.iloc[-n:-1]["products_count"].mean():.2f}")
+    col3.metric("Categories with < 10 Products", len(products_by_category[products_by_category['products_count'] < 10]))
+    col4.metric("Categories with > 100 Products", f"{len(products_by_category[products_by_category['products_count'] > 100])}")
 
 # st.markdown("""<h2 style='font-size:1.5rem; font-weight:700;'>Bar Plot for State-wise Order Distribution</h2>""", unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 fig = px.bar(
-    customers_by_state.head(len(selected_states) // 2),
-    x="customer_state",
-    y="orders_count",
-    color="customer_state",
-    title="State-Wise Order Distribution Top Half",
-    text='orders_count',
-    labels={'customer_state': 'State', 'orders_count': 'Number of Orders'},
+    products_price_by_category.head(10),
+    x="product_category_name",
+    y="price",
+    color="product_category_name",
+    title="Category Price Distribution Top Half",
+    text='price',
+    labels={'product_category_name': 'Category', 'price': 'Price'},
     template="plotly_white"
 )
 fig.update_traces(textposition='outside')
@@ -154,13 +209,13 @@ fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
 col1.plotly_chart(fig)
 
 fig = px.bar(
-    customers_by_state.tail((len(selected_states) // 2) + 1),
-    x="customer_state",
-    y="orders_count",
-    color="customer_state",
-    title="State-Wise Order Distribution Bottom Half",
-    text='orders_count',
-    labels={'customer_state': 'State', 'orders_count': 'Number of Orders'},
+    products_price_by_category.tail(10),
+    x="product_category_name",
+    y="price",
+    color="product_category_name",
+    title="Category Price Distribution Bottom Half",
+    text='price',
+    labels={'product_category_name': 'Category', 'price': 'Price'},
     template="plotly_white"
 )
 fig.update_traces(textposition='outside')
