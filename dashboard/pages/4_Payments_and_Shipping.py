f316d3ce1ca9af055d915c01684758a410f151fe
JatinY459 <14jatinyadav@gmail.com>
2025-06-30 13:28:36 +0530

    updated page 4 with payment value & volume analysis

--- dashboard/pages/4_Payments_and_Shipping.py
+++ dashboard/pages/4_Payments_and_Shipping.py
@@ -79,6 +79,13 @@ product_weight_by_group = product_weight_by_group.sort_values(by="product_weight
 product_volume_by_group = filtered_products.groupby("product_group_name")["product_volume_cm3"].mean().reset_index()
 product_volume_by_group = product_volume_by_group.sort_values(by="product_volume_cm3", ascending=False)
 
+payments_by_type = filtered_payments.groupby("payment_type")["order_id"].count().reset_index().rename(columns={'order_id': 'payments_count'})
+payments_by_type = payments_by_type.sort_values(by="payments_count", ascending=False)
+
+no_of_installments = filtered_payments[filtered_payments['payment_type'] == "credit_card"].groupby("payment_installments")["order_id"].count().reset_index().rename(columns={'order_id': 'payments_count'})
+payment_val_by_inst_count = filtered_payments[filtered_payments['payment_type'] == "credit_card"].groupby("payment_installments")["payment_value"].mean().round(2).reset_index()
+payment_val_by_inst_count = payment_val_by_inst_count.sort_values(by="payment_value", ascending=False)
+
 # dashboard content
 st.title("Payments & Shipping Analysis")
 st.markdown("""
@@ -91,37 +98,24 @@ col1.metric("Total Number of Payments", f"{filtered_payments['order_id'].nunique
 col2.metric("Number of Payment Modes", f"{filtered_payments['payment_type'].nunique():,}")
 col3.metric("Avg No. of Installments in Installment Payments", f"~{filtered_payments[filtered_payments['payment_installments'] > 1]['payment_installments'].mean():,.0f}")
 
-st.plotly_chart(
-    px.histogram(
-        filtered_order_items,
-        x="price",
-        nbins=50,
-        title="Product Price Distribution",
-        labels={"price": "Product Price"},
-        color_discrete_sequence=px.colors.qualitative.Pastel,
-        template="plotly_white"
-    ).update_layout(
-        xaxis_title="Product Price",
-        yaxis_title="Count",
-        title_x=0.5,
-        font=dict(size=14),
-        bargap=0.1,
-        bargroupgap=0.1,
-        xaxis=dict(tickformat=',.0f', ticks='outside', showline=True, linewidth=1, linecolor='black'),
-        yaxis=dict(ticks='outside', showline=True, linewidth=1, linecolor='black')
-    )
-)
+with st.expander("Payment Types/Modes Description"):
+    st.markdown("""
+- Credit Card: Payment in installments, the pre-selected number of installments is stored in `payment_installments`, with `payment_sequential` describing the total number of installments, in which it was paid in.
+- Wallet: Payments done through the digital wallet.
+- Voucher: Payments through discount vouchers (also increase `payment_sequential`), and coupons that are given for promotional, and advertising purposes.
+- Debit Card: Payments done through Debit Cards.
+""")
 
 col1, col2 = st.columns(2)
 
 fig = px.bar(
-    products_by_category.head(5),
-    x="product_category_name",
-    y="products_count",
-    color="product_category_name",
-    title="Category-Wise Product Distribution (Top 5)",
-    text='products_count',
-    labels={'product_category_name': 'Category', 'products_count': 'Number of Products'},
+    payments_by_type,
+    x='payment_type',
+    y='payments_count',
+    color='payment_type',
+    title="Number of Payments by Payment Mode",
+    text='payments_count',
+    labels={'payment_type': 'Payment Mode', 'payments_count': 'Number of Payments'},
     template="plotly_white"
 )
 fig.update_traces(textposition='outside')
@@ -129,163 +123,137 @@ fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
 col1.plotly_chart(fig)
 
 fig = px.pie(
-    pd.concat([products_by_category.head(5), pd.DataFrame({'product_category_name': ['Others'], 'products_count': [products_by_category['products_count'].sum() - products_by_category.head(5)['products_count'].sum()]})]),
-    names='product_category_name',
-    values='products_count',
-    title='Product Categories Top 5 vs Others',
-    labels={'product_category_name': 'Category', 'products_count': 'Number of Products'},
+    payments_by_type,
+    names='payment_type',
+    values='payments_count',
+    title='Payment Mode Distribution',
+    labels={'payment_type': 'Payment Mode', 'payments_count': 'Number of Payments'},
     hole=0.4,
     color_discrete_sequence=px.colors.qualitative.Pastel
 )
 fig.update_traces(textposition='inside', textinfo='percent+label', hovertemplate="<b>%{label}</b><br>Count: %{value}<br>Percentage: %{percent}")
 col2.plotly_chart(fig)
 
-with st.expander("Products Categories Top 5 vs Others Summary"):
+with st.expander("Payments Distribution by Type/Mode Summary"):
     st.markdown("""Write the summaries & Metrics/KPIs here""")
-    col1, col2, col3 = st.columns(3)
-    col1.metric("Mean no. of Products [Including Toys]", f"{products_by_category['products_count'].mean():.2f}")
-    col2.metric("Mean no. of Products [Excluding Toys]", f"{products_by_category[products_by_category['product_category_name'] !='toys']['products_count'].mean():.2f}")
-
-st.markdown("""<h2 style='font-size:1.5rem; font-weight:700;'>Categories Distribution by Order Volume & Unique Products</h2>
-            Options:""", unsafe_allow_html=True)
-selected_quantity = st.selectbox(
-    "Select quantity to show:",
-    options=["Unique Products", "Order Volume"],
-    index=0
-    )
-col1, col2, col3, col4 = st.columns([1, 3, 3, 1])
-orders_by_products = products_items_orders.groupby("product_category_name")["order_id"].count().reset_index().rename(columns={'order_id':'order_counts'})
-orders_by_products = orders_by_products.sort_values(by="order_counts", ascending=False)
-category_view = col2.radio(
-    "View Categories by:",
-    options=["Top N", "Bottom N"],
-    index=0,
-    horizontal=True
-)
-n = col3.slider(
-    f"Select number of categories to show:",
+    
+st.divider()
+st.markdown("""<h2 style='font-size:1.5rem; font-weight:700;'>Installment Count Distribution in Credit Card Payments</h2>
+            """, unsafe_allow_html=True)
+col1, col2, col3, col4, col5 = st.columns([1, 4, 1, 3, 1])
+n = col2.slider(
+    f"Select the range of Installment Counts to show:",
     min_value=1,
-    max_value=min(35, 69),
-    value=15
+    max_value=no_of_installments['payment_installments'].unique().max(),
+    value=(1,no_of_installments['payment_installments'].unique().max()),
 )
-if category_view == "Top N":
-    if selected_quantity == "Order Volume":
-            fig = px.bar(
-            orders_by_products.iloc[1:n+1],
-            x="product_category_name",
-            y="order_counts",
-            color="product_category_name",
-            title=f"Products Order Count by Categories (Top {n}) [Excluding Toys]",
-            text='order_counts',
-            labels={'product_category_name': 'Product Group', 'order_counts': 'Number of Orders'},
-            template="plotly_white"
-        )
-    elif selected_quantity == "Unique Products":
-        fig = px.bar(
-            products_by_category.iloc[1:n+1],
-            x="product_category_name",
-            y="products_count",
-            color="product_category_name",
-            title=f"Product Categories Distribution (Top {n}) [Excluding Toys]",
-            text='products_count',
-            labels={'product_category_name': 'Category', 'products_count': 'Number of Products'},
-            template="plotly_white"
-        )
-elif category_view == "Bottom N":
-    if selected_quantity == "Order Volume":
-        fig = px.bar(
-            orders_by_products.iloc[-n:-1],
-            x="product_category_name",
-            y="order_count",
-            color="product_category_name",
-            title=f"Products Order Count by Categories (Bottom {n})",
-            text='order_count',
-            labels={'product_category_name': 'Category', 'order_count': 'Number of Orders'},
-            template="plotly_white"
-        )
-    elif selected_quantity == "Unique Products":
-        fig = px.bar(
-            products_by_category.iloc[-n:-1],
-            x="product_category_name",
-            y="products_count",
-            color="product_category_name",
-            title=f"Product Categories Distribution (Bottom {n})",
-            text='products_count',
-            labels={'product_category_name': 'Category', 'products_count': 'Number of Products'},
-            template="plotly_white"
-        )
-fig.update_traces(textposition='outside')
-fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
-st.plotly_chart(fig)
-if selected_quantity == "Order Volume":
-    with st.expander("Category Order Volume Summary"):
-        st.markdown("Show Number of Orders for Toys & compare toys % to others")
-elif selected_quantity == "Unique Products":
-    with st.expander("Top Product Categories Distribution Summary"):
-        st.markdown("""Write the summaries & Metrics/KPIs here""")
-        col1, col2, col3, col4, col5 = st.columns([1, 3, 3, 3, 1])
-        col2.metric("Mean number of Products (filtered view)", f"{products_by_category.iloc[1:n+1]["products_count"].mean() if category_view=="Top N" else products_by_category.iloc[-n:-1]["products_count"].mean():.2f}")
-        col3.metric("Categories with < 10 Products", len(products_by_category[products_by_category['products_count'] < 10]))
-        col4.metric("Categories with > 100 Products", f"{len(products_by_category[products_by_category['products_count'] > 100])}")
+selected_quantity = col4.selectbox(
+    "Quantity to compare by Installment Count:",
+    options=["Number of Payments", "Payment Value"],
+    index=0
+)
+if selected_quantity == "Number of Payments":
+    fig = px.bar(
+        no_of_installments[no_of_installments["payment_installments"].between(n[0], n[1])],
+        x="payment_installments",
+        y="payments_count",
+        color="payments_count",
+        title="Number of Installments in Credit-Card Payments",
+        text='payments_count',
+        labels={'payment_installments': 'Number of Installments', 'payments_count': 'Number of Payments'},
+        template="plotly_white",
+        color_continuous_scale=px.colors.sequential.Magenta
+    )
+    fig.update_traces(textposition='outside')
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    st.plotly_chart(fig)
+    with st.expander("Payments Volume by Installment Count Summary"):
+        st.markdown("Kpis: certain number of installments have hikes user friendly numbers, we dont offer installments over 24 months, over 12 installments rarely used.")
+elif selected_quantity == "Payment Value":
+    fig = px.bar(
+        payment_val_by_inst_count[payment_val_by_inst_count["payment_installments"].between(n[0], n[1])],
+        x="payment_installments",
+        y="payment_value",
+        color="payment_value",
+        title="Payment Value in Credit Card Payments by Installments Count",
+        text='payment_value',
+        labels={'payment_installments': 'Number of Installments', 'payment_value': 'Mean Payment Value'},
+        template="plotly_white",
+        color_continuous_scale=px.colors.sequential.Magenta
+    )
+    fig.update_traces(textposition='outside')
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    st.plotly_chart(fig)
+    with st.expander("Payment Value by Installments Count Summary"):
+        st.markdown("""Kpis: certain number of installments have hikes user friendly numbers, don't offer installemtns over 24 months.
+                    -  peaks are skewed as there are only 1 order for each 22 & 23 installment count, implying, these peaks are skewed.
+                    -  in the first 12 installments, avg paymetn value is similar but increase slightly, as they are preferred, in general.
+                    - The 24 installment count has high payment value with significant number of orders suggesting, it is preferred for high payment value.""")
 
 st.divider()
+
 # st.markdown("""<h2 style='font-size:1.5rem; font-weight:700;'>Bar Plot for State-wise Order Distribution</h2>""", unsafe_allow_html=True)
-col1, col2, col3, col4 = st.columns([1, 3, 3, 1])
-category_view = col2.radio(
-    "Select categories to view",
-    options=["Top N", "Bottom N"],
-    index=0,
-    horizontal=True
+# col1, col2, col3 = st.columns([1, 2, 1])
+col1, col2 = st.columns([1, 6])
+col1.markdown("""<p style='margin-top:4rem; font-size:1.2rem; font-weight:bold;'>Options:</p>""", unsafe_allow_html=True)
+bin_count = col1.slider("Number of bins:", min_value=5, max_value=50, value=35, step=1)
+fig = px.histogram(
+    filtered_payments,
+    x=filtered_payments['payment_value'],
+    nbins=bin_count,
+    title="Payment Value Distribution",
+    template="plotly_white",
+    marginal="box",
+    opacity=0.6,
+    color_discrete_sequence=["#C363FA"],
 )
-n = col3.slider(
-    f"Select number of categories to view",
-    min_value=1,
-    max_value=min(35, 69),
-    value=15
+fig.update_layout(
+    xaxis_title="Payment Value",
+    yaxis_title="Number of Payments",
+    bargap=0.05,
+    title_x=0.5
 )
-if category_view == "Top N":
-    fig = px.bar(
-        products_price_by_category.head(n),
-        x="product_category_name",
-        y="price",
-        color="product_category_name",
-        title=f"Category Price Distribution Top {n}",
-        text='price',
-        labels={'product_category_name': 'Category', 'price': 'Price'},
-        template="plotly_white"
-    )
-    fig.update_traces(textposition='outside')
-    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
-    st.plotly_chart(fig)
+fig.update_traces(marker_line_width=1, marker_line_color="white")
+col2.plotly_chart(fig)
 
-if category_view == "Bottom N":
-    fig = px.bar(
-        products_price_by_category.tail(n),
-        x="product_category_name",
-        y="price",
-        color="product_category_name",
-        title=f"Category Price Distribution Bottom {n}",
-        text='price',
-        labels={'product_category_name': 'Category', 'price': 'Price'},
-        template="plotly_white"
-    )
-    fig.update_traces(textposition='outside')
-    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
-    st.plotly_chart(fig)
+col1, col2 = st.columns([5,3])
+fig = px.box(
+    filtered_payments,
+    x='payment_type',
+    y='payment_value',
+    points='outliers',
+    color='payment_type',
+    title="Payment Value Distribution by Payment Mode",
+    labels={
+        'payment_type': 'Payment Mode',
+        'payment_value': 'Payment Value'
+    },
+    color_discrete_sequence=px.colors.qualitative.Set2
+)
+fig.update_layout(
+    xaxis_title=None,
+    yaxis_title="Payment Value",
+    xaxis_tickangle=-45,
+    font=dict(size=13),
+    showlegend=False,
+    margin=dict(l=30, r=30, t=20, b=80)
+)
+fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey')
+col1.plotly_chart(fig)
 
-# fig = px.bar(
-#     products_price_by_category.tail(10),
-#     x="product_category_name",
-#     y="price",
-#     color="product_category_name",
-#     title="Category Price Distribution Bottom Half",
-#     text='price',
-#     labels={'product_category_name': 'Category', 'price': 'Price'},
-#     template="plotly_white"
-# )
-# fig.update_traces(textposition='outside')
-# fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
-# col2.plotly_chart(fig)
+fig = px.bar(
+    filtered_payments.groupby("payment_type")["payment_value"].mean().round(2).reset_index().sort_values(by="payment_value", ascending=False),
+    x="payment_type",
+    y="payment_value",
+    color="payment_type",
+    title="Avg Payment Value by Payment Mode",
+    text='payment_value',
+    labels={'payment_type': 'Number of Installments', 'payment_value': 'Mean Payment Value'},
+    template="plotly_white"
+)
+fig.update_traces(textposition='outside')
+fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+col2.plotly_chart(fig)
 
 st.divider()
 
